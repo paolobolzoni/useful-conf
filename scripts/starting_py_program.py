e94e3931ec254e432993d18d9f4ac79f559a2257
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2018-12-06 10:40:12 +0000

    Add docstrings to starting py program

--- scripts/starting_py_program.py
+++ scripts/starting_py_program.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python3
+
+# from __future__ import print_function #(if python2)
 
 import sys
 
+
 def eprint(*args, **kwargs):
+    """ Just like the print function, but on stderr
+    """
     print(*args, file=sys.stderr, **kwargs)
 
 def main(argv=None):
+    """ Program starting point, it can started by the OS or as normal function
+
+        If it's a normal function argv won't be None if started by the OS
+        argv is initialized by the command line arguments
+    """
     if argv is None:
         argv = sys.argv
     return 0
