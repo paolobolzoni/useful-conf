642c9b896809dbb23b70d65ba5f6954302876fb2
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2019-04-25 10:57:27 +0100

    Add on exit function to bash template script

--- scripts/bash_template.sh
+++ scripts/bash_template.sh
@@ -4,6 +4,23 @@ IFS=$'\n\t'
 
 SCRIPTNAME="`readlink -e "$0"`"
 SCRIPTDIR="`dirname "$SCRIPTNAME"`"
+STARTDIR="`dirname .`"
 
 #set -x
+
+
+on_exit_f=()
+
+# function foo {}
+# on_exit_f+=('foo')
+
+# function bar {}
+# on_exit_f+=('bar')
+
+function on_exit {
+    for i in "${!on_exit_f[@]}"; do
+        ${on_exit_f[$i]}
+    done
+}
+trap on_exit EXIT INT TERM
 
