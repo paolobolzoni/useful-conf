6229ebe4235cfee2b71cadc2f29c670bc5448e92
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2019-09-23 13:36:31 +0100

    argv[0] contains the program name in python

--- scripts/starting_py_program.py
+++ scripts/starting_py_program.py
@@ -7,21 +7,20 @@ import os
 
 
 def eprint(*args, **kwargs):
-    """ Just like the print function, but on stderr
-    """
+    ''' Just like the print function, but on stderr
+    '''
     print(*args, file=sys.stderr, **kwargs)
 
 def main(argv=None):
-    """ Program starting point, it can started by the OS or as normal function
+    ''' Program starting point, it can started by the OS or as normal function
 
         If it's a normal function argv won't be None if started by the OS
         argv is initialized by the command line arguments
-    """
-    prgname = os.path.basename(__file__) if '__file__' in globals() else 'prg'
+    '''
     if argv is None:
         argv = sys.argv
 
     return 0
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
