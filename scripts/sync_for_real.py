fdb1842a55e328f39f048c50cb1b05190055903c
Paolo Bolzoni <paolo.bolzoni2@unipr.it>
2019-10-27 11:31:55 +0100

    Sync is called slower

--- scripts/sync_for_real.py
+++ scripts/sync_for_real.py
@@ -2,7 +2,7 @@
 
 import subprocess
 import sys
-from time import time
+import time
 
 
 
@@ -12,14 +12,17 @@ def eprint(*args, **kwargs):
 
 def main():
     nr_fast = 3
+    time_length = 0.
     while nr_fast > 0:
+        time.sleep( min(time_length, 10.) )
+
         eprint('syncing... ', end='', flush=True)
-        start_t = time()
+        start_t = time.time()
         subprocess.Popen('/usr/bin/sync', stdout=None, stderr=None).wait()
-        time_length = time() - start_t
+        time_length = time.time() - start_t
         eprint('{0:0.3f}'.format(time_length))
 
-        if time_length < 0.10:
+        if time_length < 0.03:
             nr_fast = nr_fast - 1
         else:
             nr_fast = 3
